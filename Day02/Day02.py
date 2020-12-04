c1d754994e7c888a46ae6fe60c6fdbdaa2708c78
yousifkako <yousifsabah_2011@yahoo.com>
2020-12-03 20:31:02 -0500

    Day Three

--- Day02/Day02.py
+++ Day02/Day02.py
@@ -5,7 +5,7 @@ def solution_1():
     with open("Input.txt") as file:
         count = 0
         for i in file:
-            st = re.split('-|\ +|:|\n', i)
+            st = re.split('-| +|:|\n', i)
             temp = st[4].count(st[2])
             if temp >= int(st[0]) and temp <= int(st[1]):
                 count += 1
@@ -16,7 +16,7 @@ def solution_2():
     with open("Input.txt") as file:
         count = 0
         for i in file:
-            st = re.split('-|\ +|:|\n', i)
+            st = re.split('-| +|:|\n', i)
             temp_1 = st[4][int(st[0])-1]
             temp_2 = st[4][int(st[1])-1]
             if (temp_1 == st[2] and temp_2 != st[2]) or (temp_2 == st[2] and temp_1 != st[2]):
---
+++ Day03/Day03.py
@@ -0,0 +1,29 @@
+# First Puzzle
+def solution_1(x, y):
+    with open("Input.txt") as file:
+        file_read = file.read()
+    height = file_read.count('\n')
+    width = 31
+    file_read = file_read.split('\n')
+    x_count = x
+    tree_count = 0
+    for i in range(y, len(file_read), y):
+        if x_count % x == 0:
+            height -= 1
+            if file_read[i][x_count % width] == '#':
+                tree_count += 1
+        x_count += x
+    return tree_count
+
+# First Puzzle
+def solution_2():
+    slope_1 = solution_1(1, 1)
+    slope_2 = solution_1(3, 1)
+    slope_3 = solution_1(5, 1)
+    slope_4 = solution_1(7, 1)
+    slope_5 = solution_1(1, 2)
+    return slope_1 * slope_2 * slope_3 * slope_4 * slope_5
+
+
+print(solution_1(3, 1))
+print(solution_2())
